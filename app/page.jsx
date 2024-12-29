190958db1561d8b84c8efa935e126b77fff42da7
River <jahandealireza@yahoo.com>
2024-12-29 20:01:33 +0330

    FirstSectionsComplete

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import bank_icon from "./src/images/bank_icon.png" 
 import ham_icon from "./src/images/ham_icon.png"
-
+import check_icon from "./src/images/check_icon.png"
 
 
 export default function Home() {
@@ -27,12 +27,30 @@ export default function Home() {
               
             </nav>
 
-            <section className="bg-lime-300">
+            <section className="bg-purple-300  mt-[50px] flex flex-col items-center">
+
+              <div className="bg-[#262626] px-[16px] py-[8px] w-[245px] flex 
+              items-center mb-[12px] rounded-[61px]">
+                <Image src={check_icon} width="20" height="20" alt="image not found"/>
+                <p className="text-[#FFFFFF] text-[12px] ml-[4px]">No LLC Required, No Credit Check.</p>
+              </div>
+
               <p className="text-center text-[28px]">
                 Welcome to YourBank
               Empowering Your <br />
-              Financial Journey
+              <span className="text-[#CAFF33]">Financial Journey</span>
+              
               </p>
+
+              <p className="text-center text-[14px] mt-[8px]">
+              At YourBank, our mission is to provide comprehensive banking solutions 
+              that empower individuals and businesses to achieve their financial goals. 
+              We are committed to delivering personalized and innovative services that 
+              prioritize our customers' needs.
+              </p>
+
+              <button className="text-[14px] px-[24px] py-[14px] mt-[30px]
+              bg-[#CAFF33] rounded-[82px]">Open Account</button>
 
             </section>
 
